c1b9676e4289599ba720354d62e7e7f603b83816
GeekyGodess <marin.cline@gmail.com>
2019-11-25 14:44:03 +0100

    affichage fonction cos

--- app/src/main/java/com/example/superapp/BonusFragment.java
+++ app/src/main/java/com/example/superapp/BonusFragment.java
@@ -8,10 +8,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class BonusFragment extends Fragment {
+
+    private LineGraphSeries<DataPoint> series1;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bonus,null);
+        View view =  inflater.inflate(R.layout.fragment_bonus,null);
+
+
+        double x,y;
+        x = 0;
+        GraphView graph= view.findViewById(R.id.graph);
+        series1 = new LineGraphSeries<>();
+
+        int numDataPoints = 500;
+
+        for(int i = 0; i < numDataPoints; i++){
+            x = x + 0.1; // pour avoir une ligne continue
+            y = Math.sin(x);
+            series1.appendData(new DataPoint(x,y), true, 100);
+        }
+        graph.addSeries(series1);
+
+        return view;
     }
 }
